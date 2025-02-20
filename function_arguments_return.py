416ee6f369ec42a96ef0de54cfa1893ad3dea081
unknown <angelesianlloyd_bist@gmail.com>
2025-02-20 14:33:29 +0800

    Submitting Python Week 04 - Laboratory # 17

---
+++ function_arguments_return.py
@@ -0,0 +1,17 @@
+# Ian Lloyd Santiago Angeles
+# ITELEC2
+# Laboratory #17 – Guided Coding Exercise: Functions with Arguments and Return Statement
+
+def math 
+
+def circle_area(radius):
+    """Calculate and return the area of a circle given its radius."""
+
+    area = math.pi * (radius ** 2)
+
+    return area
+    
+radius_value = 5
+area_result = circle_area(radius_value)
+
+print(f"The area of a circle with radius {radius_value} is: {area_result:.2f}")
